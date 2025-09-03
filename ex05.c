fc3bbec9cc9d0b961b4709a59049d250c9b9c157
Fadwa <okfadwach2@gmail.com>
2025-09-03 14:53:27 +0200

    ddddd

---
+++ ex05.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf(" demande à l'utilisateur d'entrer une valeur : ");
+    scanf("%d", &3*x*x*x*x*x+2*x*x*x*x-5*x*x*x-x*x+7*x*x*x*x*x-6);
+    return 0;
+}
