5dba800e5d42d6e1da318000dfa0198c940824f2
Fadwa <okfadwach2@gmail.com>
2025-09-03 12:15:38 +0200

    fff

---
+++ ex01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf( *n);
+    printf( *n);
+    printf( *n);
+    printf(* *n);
+    printf( Git-2.51.0-64-bit.exe Microsoft Edge.lnk MinGW Installer.lnk Visual Studio Code.lnk desktop.ini ex05.c fdw msys2-x86_64-20250830.exe *n);
+    printf( *n);
+    return 0;
+} 
