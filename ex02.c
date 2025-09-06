f7db4f220888476a158f7b9fac99297c075c481b
fadwa <fadwafch388@gmail.com>
2025-09-06 04:52:11 +0200

    on

--- ex02.c
+++ ex02.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 int main()
 {
     float r = 10;
     float v;
-    printf(%fn,  v = 4.0f/3.0f*3.14156*r*r*r);
-    return 0;
+
+    v = 4.0f/3.0f*3.14156*r*r*r;
+    printf("%f", v);
 }
--- ex03.c
+++ ex03.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+
 int main()
 {
     float r;
     float v;
 
-    printf(enter le rayon de la sphère : );
-    scanf(%f, &r);
-
+    printf("entrez le rayon de la sphère : ");
+    scanf("%f", &r);
+    
     v = 4.0f/3.0f*3.14156*r*r*r;
-
-    printf(%f, v);
-    return 0;
+    printf("%f", v);
 }
